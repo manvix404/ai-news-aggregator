7cc33b5aa351e7e1deff16ec2904f0a81c8689da
manvix404 <manvik55@gmail.com>
2026-01-04 21:56:40 +0530

    Added docker and postgres table create

---
+++ app/database/create_tables.py
@@ -0,0 +1,8 @@
+import sys
+from pathlib import Path
+from app.database.models import Base
+from app.database.connection import engine
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    print("Tables created successfully")
